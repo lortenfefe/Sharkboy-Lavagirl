17061f10c916f651b8a459b885a540bdf96d33b6
lortenfefe <66171193+lortenfefe@users.noreply.github.com>
2021-02-17 15:16:16 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ function setup() {
 
 function draw() {
   background(220);
+  fill(97, 123, 255)
+  rect(0, 0, width/2, height)
+  fill(255, 66, 66)
+  rect(width/2, 0, width, height)
   strokeWeight(5)
   line(width/2, 0, width/2, height)
 }
